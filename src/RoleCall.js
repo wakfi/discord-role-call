5af9e307a681ab5e274eacd9d91f05f35de0799b
Walker Gray <55608093+wakfi@users.noreply.github.com>
2021-01-18 21:59:04 -0800

    Add separate collections for role-emoji pairs and roles
    roleEmojiPairs is mapped by emoji, roles is mapped by Role ID

--- src/RoleCall.js
+++ src/RoleCall.js
@@ -28,7 +28,7 @@ class RoleCall extends EventEmitter
 	/**
 	 * Create a new RoleCall instance for a given client and configuration options
 	 * @param {Discord.Client} [client] Represents the bot client
-	 * @param {RoleCallConfig} [config] Configuration/setup information for RoleCall instance
+	 * @param {RoleCallConfig} [config] Configuration/setup information for RoleCall instance 
 	 */
 	constructor(client,config) 
 	{
@@ -38,26 +38,28 @@ class RoleCall extends EventEmitter
 		this.guild.channels.cache.get(config.channelId).messages.fetch(config.messageId).then(theMessage =>
 		{
 			this.message = theMessage; // because we need the message object, we have to retrieve it, and because internet, this takes time. so we have to wait and set it here
-			this.roles = new Collection(); // @type: Collection<Snowflake, Role> where Snowflake is the snowflake of the emoji that is associate with the role
-			this.reactions = new Collection(); // @type: Collection<Snowflake, MessageReaction> passed in as emoji resolvables
+			this.roles = new Collection(); // @type: Collection<Snowflake, Role> where the Snowflake is the ID of the role 
+			this.roleEmojiPairs = new Collection(); // @type: Collection<Emoji, Role> where Emoji is the emoji identifier that is associated with the role 
+			this.reactions = new Collection(); // @type: Collection<Snowflake, MessageReaction> passed in as emoji resolvables								  
 			const reactArr = []; // this is a local variable that will be used during construction
-
+			
 			// Set roles collection. Collection is an extension of the JavaScript Map object with expanded functionality, primarily for mapping ID (aka snowflake) to object
-			config.roleInputArray.map(roleToCall =>
-			{
+			config.roleInputArray.map(roleToCall => 
+			{ 
 				this.guild.roles.fetch();
 				if(this.guild.roles.cache.has(roleToCall.role))
 				{
-					this.roles.set(roleToCall.emoji, this.guild.roles.cache.get(roleToCall.role));
+					this.roles.set(roleToCall.role, this.guild.roles.cache.get(roleToCall.role));
+					this.roleEmojiPairs.set(roleToCall.emoji, this.guild.roles.cache.get(roleToCall.role));
 				} else {
 					throw new Error(`${this.guild} does not have role resolvable with ${roleToCall.role}`); //change this to log instead of throwing if you don't want object construction to break, in other words, if you don't want this to be fatal to the instantiation
 				}
 			});
-
+			
 			// Collect matching reaction objects from existing reactions
-			this.message.reactions && this.message.reactions.cache.forEach(reaction =>
+			this.message.reactions && this.message.reactions.cache.forEach(reaction => 
 			{
-				if(this.roles.has(reaction.emoji.name))
+				if(this.roleEmojiPairs.has(reaction.emoji.name))
 				{
 					this.reactions.set(reaction.emoji.name, reaction);
 				}
@@ -66,8 +68,8 @@ class RoleCall extends EventEmitter
 			// Fill in any remaining reactions for emojis not found as existing reactions in order to fill out collections
 			if(this.reactions.size < config.roleInputArray.length)
 			{
-				config.roleInputArray.map(roleToCall =>
-				{
+				config.roleInputArray.map(roleToCall => 
+				{ 
 					if(!this.reactions.has(roleToCall.emoji))
 					{
 						let emoji = roleToCall.emoji.includes(`<`) ? guild.emoji.names.cache.get(roleToCall.emoji) : roleToCall.emoji;
@@ -81,7 +83,7 @@ class RoleCall extends EventEmitter
 			}
 			
 			// Wait until client finishes adding its own reactions before adding the reaction listeners, so that it doesnt try to handle iteself
-			Promise.all(reactArr).then(async done =>
+			Promise.all(reactArr).then(async done => 
 			{
 				this.__retryQueue = 0;
 				this.client.setMaxListeners(this.client.getMaxListeners() + 1);
@@ -117,7 +119,7 @@ class RoleCall extends EventEmitter
 			if(!user || user.bot) return;
 			// Check which type of event it is before emitting
 			const member = this.client.guilds.cache.get(this.guild.id).members.cache.get(user.id);
-			const role = this.roles.get(reaction.emoji.name);
+			const role = this.roleEmojiPairs.get(reaction.emoji.name);
 			if(packet.t === 'MESSAGE_REACTION_ADD')
 			{
 				// Adds the currently reacting user to the reaction's ReactionUserManager
@@ -132,7 +134,7 @@ class RoleCall extends EventEmitter
 	} 
 	
 	// @throws Error
-	// Default add handler provided for convenience. Call this if you don't want to write your own handler or don't need to add any logic.
+	// Default add handler provided for convenience. Call this if you don't want to write your own handler or don't need to add any logic. 
 	// You can also call it after your own added logic. Returns a promise with the edited member, or an error if one is thrown. When you
 	// catch the error, output its .stack property to get the full set of info, including locations in your program. I'm still working on
 	// making this more robust, right now it runs into issues with too many listeners due to the promises building up if you spam it pretty
@@ -173,7 +175,7 @@ class RoleCall extends EventEmitter
 	// @throws Error
 	// Default remove handler provided for convenience. Call this if you don't want to write your own handler or don't need to add any logic.
 	// You can also call it after your own added logic. Returns a promise with the edited member, or an error if one is thrown. When you
-	// catch the error, output its .stack property to get the full set of info, including locations in your program. I'm still working on
+	// catch the error, output its .stack property to get the full set of info, including locations in your program. I'm still working on 
 	// making this more robust, right now it runs into issues with too many listeners due to the promises building up if you spam it pretty
 	// good.
 	removeRole(member, role, retry = false)
@@ -208,15 +210,15 @@ class RoleCall extends EventEmitter
 			}
 		});
 	}
-
+	
 	// Remove reaction & role, using role to find the reaction. This
 	// is useful for creating poll-button type functionality, where
-	// only one of a set of options can be selected at a time and
+	// only one of a set of options can be selected at a time and 
 	// choosing a different option will deselect the previous option.
 	// Returns the promise from RoleCall#removeRole
 	removeReaction(member, role, retry = false)
 	{
-		const emoji = this.roles.findKey(val => val.id == role.id);
+		const emoji = this.roleEmojiPairs.findKey(val => val.id == role.id);
 		const reaction = this.reactions.get(emoji);
 		reaction.users.remove(member).catch(e=>console.error(e.stack)); // An error here is non-fatal, lets removeRole figure that out
 		return this.removeRole(member, role, retry);
